2d1f018e7b3a90c8f2265d5e3824ac32ebc398b6
Vivek Kumar <91492060+vivkr0207@users.noreply.github.com>
2023-01-06 22:43:14 +0530

    Add files via upload

---
+++ 31function-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void printmessage(){
+    printf("Hello! I am Vivek\n");
+    printf("This is my boring program\n");
+}
+main(){
+    printmessage();
+    printmessage();
+    printf("Kumar\n");
+    printmessage();
+}
